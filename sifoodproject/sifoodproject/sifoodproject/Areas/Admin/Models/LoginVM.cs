5ffe1a04d5f0037a1370f476e8f0bcd32c429174
Brad <brad881112@gmail.com>
2023-12-16 14:14:43 +0800

    new commit

--- sifoodproject/sifoodproject/sifoodproject/Areas/Admin/Models/LoginVM.cs
+++ sifoodproject/sifoodproject/sifoodproject/Areas/Admin/Models/LoginVM.cs
@@ -4,10 +4,8 @@ namespace sifoodproject.Areas.Admin.Models
 {
     public class LoginVM
     {
-        [Required(ErrorMessage = "請輸入登入帳號")]
         public string? Account { get; set; }
 
-        [Required(ErrorMessage = "請輸入登入密碼")]
         public string? Password { get; set; }
     }
 }
--- sifoodproject/sifoodproject/sifoodproject/Areas/Stores/Models/StoreLoginVM.cs
+++ sifoodproject/sifoodproject/sifoodproject/Areas/Stores/Models/StoreLoginVM.cs
@@ -4,10 +4,8 @@ namespace sifoodproject.Areas.Stores.Models
 {
     public class StoreLoginVM
     {
-        [Required(ErrorMessage = "請輸入登入帳號")]
         public string? StoreAccount { get; set; }
 
-        [Required(ErrorMessage = "請輸入登入密碼")]
         public string? SetPassword { get; set; }
     }
 }
